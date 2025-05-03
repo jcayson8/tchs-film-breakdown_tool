f24e9e8f390ac8cbdac84ab841a305421a0a2183
jcayson8 <jcaysontchs@gmail.com>
2025-05-03 00:23:21 -0400

    Delete tchs_film_breakdown_repo directory

--- tchs_film_breakdown_repo/frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const App = () => <h1>TCHS Football Film Breakdown Tool</h1>;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
